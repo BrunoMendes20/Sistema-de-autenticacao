84d0b6178bb8cab2955efa1b90f19c1f53d47b78
Bruno <brunoomendes20@gmail.com>
2025-12-22 12:04:03 -0300

    refactor(ui): improve auth card with divider and remember me support

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,32 +1,44 @@
 
     <div class="container mt-5  d-flex justify-content-center align-items-center">
         <div class="col-md-6 col-sm-8">
-            <div class="card p-5">
+            <div class="card p-5 de-flex flex-column ">
 
                 <div class="row mt-5 justify-content-center">
                     <div class="col-md-10 col-12">
+                        
+                        
+                        {{ $slot ?? null }}
 
-                      {{ $slot }}
-                       
-                        <hr>
-                        <p class="text-center">
-                            {{ $footer }}
-                        </p>
-
-                        <div class="d-flex align-items-center my-4">
-                            <hr class="flex-grow-1">
-                            {{ $orText }}
-                            <hr class="flex-grow-1">
-
+                        
+                        <div class="text-start mt-3">
+                            {{$rememberMe ?? null}}
+                            
                         </div>
+
+                        @if (isset($divider))
+                       
+                            <div class="d-flex align-items-center my-4">
+                                <hr class="flex-grow-1">
+                                    {{ $divider ?? null }}
+                                <hr class="flex-grow-1">
+                                
+                            </div>
+                            
+                        @endif
+
                         
                         <div class="mt-4 text-center">
-                          {{ $authButton}}
+                            {{ $authButton ?? null }}
                         </div>
-
+                        
+                       
+                        
                     </div>
-
+                    
                 </div>
+                <p class="text-center mt-auto ">
+                    {{ $footer  ?? null }}
+                </p>
 
             </div>
         </div>

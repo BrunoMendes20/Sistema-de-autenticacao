1a37f06a9351a5f25edaa5e295cf5d94c1b66c5a
Bruno <brunoomendes20@gmail.com>
2025-12-22 12:00:33 -0300

    feat(auth): add password confirmation field to register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,13 +8,13 @@
             
             <div class="mb-3">
                 <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" name="name"  value="{{ old('name') }}" required>
+                <input type="text" class="form-control"  name="name"  value="{{ old('name') }}" required>
 
             </div>
 
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email"  value="{{ old('email') }}" required>
+                <input type="email" class="form-control"  name="email"  value="{{ old('email') }}" required>
             </div>
 
              @error('email')
@@ -26,33 +26,34 @@
 
             <div class="mb-3">
                 <label for="password" class="form-label">Senha</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+                <input type="password" class="form-control"  name="password" required>
+            </div>
+            
+            @error('password')
+            <div class="alert alert-danger d-block fade show auto-close">
+                {{ $message }}
+            </div>
+            
+            @enderror
+            
+            <div class="mb-3">
+                <label for="password_confirmation" class="form-label">Confirma Senha</label>
+                <input type="password" class="form-control" name="password_confirmation" required>
             </div>
 
-             @error('password')
+            @error('password_confirmation')
                 <div class="alert alert-danger d-block fade show auto-close">
                     {{ $message }}
                 </div>
-                
-            @enderror
             
+            @enderror
+
             <button type="submit" class="btn w-100">Inscreva-se</button>
         </form>
 
         <x-slot:footer>
             Já tem uma conta? <a href="{{route('login')}}">Faça Login</a>
         </x-slot:footer>
-
-        <x-slot:orText>
-            <span class="mx-2 text-muted">OU</span>
-        </x-slot:orText>
-
-        <x-slot:authButton>
-
-            <a href="{{ route('google.login') }}" class="btn btn-google">
-                <i class="fa-brands fa-google"></i> 
-            </a>
-        </x-slot:authButton>
     </x-auth-card>
 @endsection
 

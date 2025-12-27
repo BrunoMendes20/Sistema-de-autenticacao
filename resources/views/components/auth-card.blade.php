b865baf99b734fdfe0bd3b7b282b800ea940f35c
Bruno <brunoomendes20@gmail.com>
2025-12-27 18:33:04 -0300

    fix(ui): remove dynamic remember me from auth card

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,42 +1,36 @@
+<div class="container mt-5  d-flex justify-content-center align-items-center">
+    <div class="col-md-6 col-sm-8">
+        <div class="card p-5 de-flex flex-column ">
 
-    <div class="container mt-5  d-flex justify-content-center align-items-center">
-        <div class="col-md-6 col-sm-8">
-            <div class="card p-5 de-flex flex-column ">
+            <div class="row mt-5 justify-content-center">
+                <div class="col-md-10 col-12">
+                             
+                    {{ $slot ?? null }}
 
-                <div class="row mt-5 justify-content-center">
-                    <div class="col-md-10 col-12">
-                        
-                        
-                        {{ $slot ?? null }}
-
-
-                        @if (isset($divider))
+                    @if (isset($divider))
                        
-                            <div class="d-flex align-items-center my-4">
-                                <hr class="flex-grow-1">
-                                    {{ $divider ?? null }}
-                                <hr class="flex-grow-1">
+                        <div class="d-flex align-items-center my-4">
+                            <hr class="flex-grow-1">
+                                {{ $divider ?? null }}
+                            <hr class="flex-grow-1">
                                 
-                            </div>
-                            
-                        @endif
-
-                        
-                        <div class="mt-4 text-center">
-                            {{ $authButton ?? null }}
                         </div>
-                        
-                       
-                        
+                            
+                    @endif
+  
+                    <div class="mt-4 text-center">
+                        {{ $authButton ?? null }}
                     </div>
-                    
+                             
                 </div>
+                    
+            </div>
                 <p class="text-center mt-auto ">
                     {{ $footer  ?? null }}
                 </p>
 
-            </div>
         </div>
-        
     </div>
+        
+</div>
 

7cd682e24f3ebf9a97bab1a9b72f903dc056649e
Bruno <brunoomendes20@gmail.com>
2025-12-27 18:17:24 -0300

    feat(auth): override default password reset mail notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,29 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomResetPassword;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordBase;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
+    use HasFactory, Notifiable;
+
     // User model code here
     protected $fillable = ['name', 'email', 'password', 'google_id'];
     protected $hidden = ['password', 'remember_token'];
-}   
+
+    protected static function booted()
+    {
+        ResetPasswordBase::toMailUsing(function ($notifiable, $token) {
+            return (new CustomResetPassword($token))
+               ->toMail($notifiable);
+        });
+        
+    }
+    
+
+}
